652bb0a75411dc5eee87a364a3818184a2fb206b
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-12-18 23:02:38 -0300

    agregando orden a los enlaces

--- src/components/DevTree.tsx
+++ src/components/DevTree.tsx
@@ -1,13 +1,22 @@
 import { Link, Outlet } from "react-router-dom"
 import NavigationTabs from "./NavigationsTabs"
 import { Toaster } from "sonner"
-import { User } from "../types"
+import { SocialNetwork, User } from "../types"
+import { useEffect, useState } from "react"
+import DevTreeLink from "./DevTreeLink"
 
 type DevTreeProps = {
     data: User,
 }
 
 const DevTree = ({data} : DevTreeProps) => {
+
+    const [enabledLinks, setEnablesLinks] = useState<SocialNetwork[]>(JSON.parse(data.links).filter((item : SocialNetwork)  => item.enabled));
+
+    useEffect(() => {
+        setEnablesLinks(JSON.parse(data.links).filter((item : SocialNetwork)  => item.enabled))
+    }, [data])
+
   return (
     <>
             <header className="bg-slate-800 py-5">
@@ -47,6 +56,12 @@ const DevTree = ({data} : DevTreeProps) => {
                             <p className="text-4xl text-center text-white">{data.handle}</p>
                             {data.image && <img src={data.image} alt="Imagen perfil" className="mx-auto max-w-[250px]"/>}
                             <p className="text-center text-lg font-black text-white">{data.description}</p> 
+                            <p className="text-white">Iconos Aqui</p>
+                            <div className="mt-20 flex flex-col gap-5 ">
+                                {enabledLinks.map(link => (
+                                     <DevTreeLink key={link.name} link={link} />
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </main>
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { social } from "../data/social"
 import DevTreeInput from "../components/DevTreeInput"
 import { isValidUrl } from "../utils"
 import { toast } from "sonner"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { updateProfile } from "../api/DevTreeApi"
-import { User } from "../types"
+import { SocialNetwork, User } from "../types"
 
 const LinkTreeView = () => {
 
@@ -24,10 +24,25 @@ const LinkTreeView = () => {
     }
   })
 
+  useEffect(() => {
+    const updatedData = devTreeLinks.map(item => {
+      const userLink = JSON.parse(user.links).find((link : SocialNetwork)  => link.name === item.name)
+      if (userLink) {
+        return { ...item, url: userLink.url, enabled: userLink.enabled}
+      }
+      return item
+    })
+
+    setDevTreeLinks(updatedData)
+  }, [])
+
   const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
     const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value} : link)
     setDevTreeLinks(updatedLinks)
   }
+
+
+  const links : SocialNetwork[] = JSON.parse(user.links)
 
   const handleEnableLink = (socialNetwork : string) => {
     const updatedLinks = devTreeLinks.map(link => {
@@ -43,10 +58,56 @@ const LinkTreeView = () => {
     })
     setDevTreeLinks(updatedLinks)
 
+    let updatedItems : SocialNetwork[] = [];
+
+    const selectedSocialNetwork = updatedLinks.find(link => link.name === socialNetwork)
+    if (selectedSocialNetwork?.enabled) {
+      const id = links.filter(link => link.id).length + 1;
+      if (links.some(link => link.name === socialNetwork)) {
+        updatedItems = links.map(link => {
+          if (link.name === socialNetwork) { 
+            return {
+              ...link,
+              enabled: true,
+              id
+            }
+          } else {
+            return link
+          }
+        })
+      } else {
+        const newItem = {
+          ...selectedSocialNetwork,
+          id
+        }
+        updatedItems = [...links, newItem]
+      }
+    } else {
+      const indexToUpdate = links.findIndex(link => link.name === socialNetwork)
+      updatedItems = links.map(link => {
+        if (link.name === socialNetwork) {
+          return {
+            ...link,
+            id: 0,
+            enabled: false,
+          };
+        } else if (link.id > indexToUpdate) {
+          return {
+            ...link,
+            id: link.id - 1,
+          };
+        }
+        // Si no cumple ninguna condición, devuelve el enlace sin cambios
+        return link;
+      });
+      
+    }
+
+    // Almacenar en la base de datos
     queryClient.setQueryData(['user'], (prevData : User) => {
       return {
         ...prevData,
-        links: JSON.stringify(updatedLinks)
+        links: JSON.stringify(updatedItems)
       }
     })
   }
